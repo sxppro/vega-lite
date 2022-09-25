8d94721aa57d9cf3965c2ea269cb6d69e55b1e30
Josh <j.p.lu@outlook.com>
2022-09-25 21:51:22 +1000

    feat: update chart components to use Box

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { visualise } from '../utils/charts';
+import { Box } from '@chakra-ui/react';
 
 const BarChart = () => {
   useEffect(() => {
     visualise('#bar-chart', '/api/barchart').catch(console.error);
   }, []);
-  return <div id="bar-chart" className="bar-chart"></div>;
+  return <Box id="bar-chart" className="bar-chart"></Box>;
 };
 
 export default BarChart;
--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { visualise } from '../utils/charts';
+import { Box } from '@chakra-ui/react';
 
 const Map = () => {
   useEffect(() => {
     visualise('#map', '/api/map').catch(console.error);
   }, []);
-  return <div id="map" className="map"></div>;
+  return <Box id="map" className="map"></Box>;
 };
 
 export default Map;
