4c37e3f38b956ee0551baaec18465bf8ee307c8b
Josh <j.p.lu@outlook.com>
2022-10-10 01:53:45 +1100

    feat: add placeholder items

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,44 @@ export default function Home() {
 
           <VStack spacing={4} w="100%">
             <StatsHighlight average={'80.0%'} best={'85.0%'} />
+            <Flex alignContent={'center'} flexDir={'row'} w="100%" h="640px">
+              <Box borderWidth={'1px'} borderRadius={12} w="50%" mr={2} p={4}>
+                <Box borderWidth={'1px'} w="100%" h="60%">
+                  <Center h="100%">
+                    <Text>Placeholder</Text>
+                  </Center>
+                </Box>
+                <Heading size={'lg'} mt={2}>
+                  Chart Title
+                </Heading>
+                <Text fontSize={'lg'} mt={2}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.
+                </Text>
+              </Box>
+              <Box borderWidth={'1px'} borderRadius={12} w="50%" ml={2} p={4}>
+                <Box borderWidth={'1px'} w="100%" h="60%">
+                  <Center h="100%">
+                    <Text>Placeholder</Text>
+                  </Center>
+                </Box>
+                <Heading size={'lg'} mt={2}>
+                  Chart Title
+                </Heading>
+                <Text fontSize={'lg'} mt={2}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.
+                </Text>
+              </Box>
+            </Flex>
           </VStack>
 
           <Heading
@@ -96,13 +134,24 @@ export default function Home() {
                   dolore eu fugiat nulla pariatur.
                 </Text>
               </Box>
-              <Box
-                borderWidth={'1px'}
-                borderRadius={12}
-                w="50%"
-                ml={2}
-                p={4}
-              ></Box>
+              <Box borderWidth={'1px'} borderRadius={12} w="50%" ml={2} p={4}>
+                <Box borderWidth={'1px'} w="100%" h="60%">
+                  <Center h="100%">
+                    <Text>Placeholder</Text>
+                  </Center>
+                </Box>
+                <Heading size={'lg'} mt={8}>
+                  Chart Title
+                </Heading>
+                <Text fontSize={'lg'} mt={2}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.
+                </Text>
+              </Box>
             </Flex>
           </VStack>
         </Center>
