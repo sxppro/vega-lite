6f6c7b30b6f5eb6729d8b24b73e587912354e7a2
Josh <j.p.lu@outlook.com>
2022-10-09 23:28:21 +1100

    fix: component naming err

--- pages/week-9.js
+++ pages/week-9.js
@@ -1,10 +1,10 @@
 import { Container } from '@chakra-ui/react';
-import Map from '../components/Map';
+import AirportRankingMap from '../components/AirportRankingMap';
 
 const Week9 = () => {
   return (
     <Container maxW="container.xl" centerContent h="calc(100vh - 64px)">
-      <Map />
+      <AirportRankingMap />
     </Container>
   );
 };
