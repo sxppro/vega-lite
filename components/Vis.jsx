d9224d80b3e3b110004a3bad3a9372795c3aac7d
Josh <j.p.lu@outlook.com>
2022-10-15 01:14:35 +1100

    feat: update visualisations and add tabs to time section

--- components/Vis.jsx
+++ components/Vis.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { visualise } from '../utils/charts';
 import { Box, Center, Spinner } from '@chakra-ui/react';
 
-const Vis = ({ visId }) => {
+const Vis = ({ visId, height = '360px' }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const Vis = ({ visId }) => {
   }, [visId]);
 
   return (
-    <Box id={visId} w="100%" h={{ base: '256px', md: '360px' }}>
+    <Box id={visId} w="100%" h={{ base: '256px', md: height }}>
       {loading ? (
         <Center h={'inherit'}>
           <Spinner size={'xl'} />
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,16 @@ import {
   VStack,
   Box,
   Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
 } from '@chakra-ui/react';
+import Airlines from '../components/Airlines';
+import Airports from '../components/Airports';
 import StatsHighlight from '../components/StatsHighlight';
+import TimePeriod from '../components/TimePeriod';
 import Vis from '../components/Vis';
 
 export default function Home() {
@@ -25,171 +33,12 @@ export default function Home() {
             US Flight Delays
           </Heading>
           <Divider />
-          <Heading
-            p={'8'}
-            size="2xl"
-            bgGradient="linear(to-tr, #f36364, #f292ed)"
-            bgClip="text"
-          >
-            Airports
-          </Heading>
 
-          <VStack spacing={8} w="100%">
-            <Text fontStyle="italic">
-              All diagrams refer to data for year to date July 2022, unless
-              otherwise specified{' '}
-            </Text>
-            <StatsHighlight average={'79.2%'} best={'88.2%'} />
-            <Stack
-              alignContent={'center'}
-              direction={['column', 'row']}
-              spacing={4}
-              w="100%"
-            >
-              <Box
-                _hover={{
-                  boxShadow: 'md',
-                  transition: 'box-shadow 0.1s linear',
-                }}
-                borderWidth={'1px'}
-                borderRadius={12}
-                p={4}
-              >
-                <Vis visId={'airportFlightsMap'} />
-                <Heading size={'lg'} pt={2}>
-                  Airports by Number of Arriving Flights
-                </Heading>
-                <Text fontSize={'lg'} pt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-              <Box borderWidth={'1px'} borderRadius={12} p={4}>
-                <Vis visId={'bestAirports'} />
-                <Heading size={'lg'} mt={2}>
-                  Top 10 Airports by On-time Arrivals
-                </Heading>
-                <Text fontSize={'lg'} mt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-            </Stack>
-          </VStack>
+          <Airports />
 
-          <Heading
-            p={'8'}
-            size="2xl"
-            bgGradient="linear(to-tr, #ab2aed, #fffbaf)"
-            bgClip="text"
-          >
-            Airlines
-          </Heading>
+          <Airlines />
 
-          <VStack spacing={8} w="100%">
-            <StatsHighlight average={'80.0%'} best={'85.0%'} />
-            <Stack
-              alignContent={'center'}
-              direction={['column', 'row']}
-              spacing={4}
-              w="100%"
-            >
-              <Box borderWidth={'1px'} borderRadius={12} p={4}>
-                <Vis visId={'airlineOntimeRanking'} />
-                <Heading size={'lg'} mt={2}>
-                  Chart Title
-                </Heading>
-                <Text fontSize={'lg'} mt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-              <Box borderWidth={'1px'} borderRadius={12} p={4}>
-                <Box borderWidth={'1px'} w="100%">
-                  <Center minH="360px">
-                    <Text>Placeholder</Text>
-                  </Center>
-                </Box>
-                <Heading size={'lg'} mt={2}>
-                  Chart Title
-                </Heading>
-                <Text fontSize={'lg'} mt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-            </Stack>
-          </VStack>
-
-          <Heading
-            p={'8'}
-            size="2xl"
-            bgGradient="linear(to-tr, #f7a2a1, #ffed00)"
-            bgClip="text"
-          >
-            Time Period
-          </Heading>
-
-          <VStack spacing={8} w="100%">
-            <Stack
-              alignContent={'center'}
-              direction={['column', 'row']}
-              spacing={4}
-              w="100%"
-            >
-              <Box borderWidth={'1px'} borderRadius={12} p={4}>
-                <Vis visId={'yearlyDelayCause'} />
-                <Heading size={'lg'} mt={2}>
-                  Yearly Flight Delay Causes
-                </Heading>
-                <Text fontSize={'lg'} mt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-            </Stack>
-            <Stack
-              alignContent={'center'}
-              direction={['column', 'row']}
-              spacing={4}
-              w="100%"
-            >
-              <Box borderWidth={'1px'} borderRadius={12} p={4}>
-                <Vis visId={'carrierOntimeHeatmap'} />
-                <Heading size={'lg'} mt={8}>
-                  Monthly On-time Arrivals (2005-2021)
-                </Heading>
-                <Text fontSize={'lg'} mt={2}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur.
-                </Text>
-              </Box>
-            </Stack>
-          </VStack>
+          <TimePeriod />
         </Center>
       </Container>
     </main>
